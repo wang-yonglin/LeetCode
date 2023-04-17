08b96a62a4b8835f83635e07974a9b3497009ca4
wang-yonglin <wangyonglin_2020@163.com>
2023-04-18 00:45:10 +0800

    leetcode876题：链表中间节点

---
+++ src/cn/leetcode/question876/App.java
@@ -0,0 +1,31 @@
+package cn.leetcode.question876;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4};
+
+        ListNode list = initListNode(arr);
+
+        ListNode node = Solution1.middleNode(list);
+
+        System.out.println(node.val);
+    }
+
+
+    private static ListNode initListNode(int [] arr) {
+
+        ListNode dummy = new ListNode(-1);
+
+        ListNode p = dummy;
+
+        for (int i = 0; i < arr.length; i++) {
+            p.next = new ListNode(arr[i]);
+            p = p.next;
+        }
+
+        return dummy.next;
+    }
+
+}
---
+++ src/cn/leetcode/question876/Solution1.java
@@ -0,0 +1,20 @@
+package cn.leetcode.question876;
+
+
+public class Solution1 {
+
+
+    public static ListNode middleNode(ListNode head) {
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        return slow;
+    }
+
+}
